cddf1cbab9456214c1f4e0129359c69d58ec9188
nihaoworld <89476288+realnihaoworld@users.noreply.github.com>
2022-10-11 22:06:02 -0500

    basic flashcard screen template

---
+++ src/screens/FlashcardScreen.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function FlashcardScreen() {
+  return (
+    <View>
+      <Text>FlashcardScreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
